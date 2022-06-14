2b8b26bce744d8027f3f31d8e7c23ff34c55ca37
Rubix327 <yarik.cool1@mail.ru>
2022-06-14 12:52:59 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AntiItemMove
+A plugin that prevents players to move vanilla and custom items to another inventory.
---
+++ src/main/java/me/rubix327/antiitemmove/AntiItemMoveMain.java
@@ -0,0 +1,10 @@
+package me.rubix327.antiitemmove;
+
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+public class AntiItemMoveMain extends SimplePlugin {
+    @Override
+    protected void onPluginStart() {
+
+    }
+}
